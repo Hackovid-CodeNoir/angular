e3da874b0902a7700b49ff96f7f99cf4a14be56b
Leonides Muguercia Tellez <leonimuguercia5@gmail.com>
2020-04-07 16:14:16 +0200

    Laying out the top head mobile-first

--- projects/market-kzero/src/app/global-area/global-area.module.ts
+++ projects/market-kzero/src/app/global-area/global-area.module.ts
@@ -7,6 +7,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
 
 @NgModule({
   declarations: [GlobalAreaComponent],
@@ -18,6 +19,7 @@ import { MatListModule } from '@angular/material/list';
     MatSidenavModule,
     MatIconModule,
     MatListModule,
+    MatMenuModule,
   ],
   exports: [GlobalAreaComponent],
 })
