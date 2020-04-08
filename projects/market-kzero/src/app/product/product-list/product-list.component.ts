f78a14ceece3ffa2ac6b94318082823c290522d7
ERNI\cagi <gilbe.cao@gmail.com>
2020-04-08 16:10:42 +0200

    move sellers to a new file

--- projects/market-kzero/src/app/product/product-list/product-list.component.ts
+++ projects/market-kzero/src/app/product/product-list/product-list.component.ts
@@ -1,50 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { seller } from './sellers';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.scss'],
 })
-export class ProductListComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
-
-  public sellers = [
-    {
-      name: 'L´hort de Lourdes',
-      productName: 'Cistelles ecológiques',
-      img: 'assets/img/cesta.png',
-      bio:
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium sequi fuga delectus quos expedita ut ipsam similique et ea, repellend',
-    },
-    {
-      name: 'Can Manel',
-      productName: 'Fruitas i Verdures',
-      img: 'assets/img/frutas.png',
-      bio:
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium sequi fuga delectus quos expedita ut ipsam similique et ea, repellend',
-    },
-    {
-      name: 'Verdures Carmen',
-      productName: 'Verdures variades',
-      img: 'assets/img/zanahorias.png',
-      bio:
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium sequi fuga delectus quos expedita ut ipsam similique et ea, repellend',
-    },
-    {
-      name: 'La Masia',
-      productName: 'Llegums i bollets',
-      img: 'assets/img/champiñones.png',
-      bio:
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium sequi fuga delectus quos expedita ut ipsam similique et ea, repellend',
-    },
-    {
-      name: 'L`hort d´en Lluis',
-      productName: 'Frutas',
-      img: 'assets/img/platano.png',
-      bio:
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium sequi fuga delectus quos expedita ut ipsam similique et ea, repellend',
-    },
-  ];
+export class ProductListComponent {
+  public sellers = seller;
 }
---
+++ projects/market-kzero/src/app/product/product-list/sellers.ts
@@ -0,0 +1,37 @@
+export const seller = [
+  {
+    name: 'L´hort de Lourdes',
+    productName: 'Cistelles ecológiques',
+    img: 'assets/img/cesta.png',
+    bio:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium sequi fuga delectus quos expedita ut ipsam similique et ea, repellend',
+  },
+  {
+    name: 'Can Manel',
+    productName: 'Fruitas i Verdures',
+    img: 'assets/img/frutas.png',
+    bio:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium sequi fuga delectus quos expedita ut ipsam similique et ea, repellend',
+  },
+  {
+    name: 'Verdures Carmen',
+    productName: 'Verdures variades',
+    img: 'assets/img/zanahorias.png',
+    bio:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium sequi fuga delectus quos expedita ut ipsam similique et ea, repellend',
+  },
+  {
+    name: 'La Masia',
+    productName: 'Llegums i bollets',
+    img: 'assets/img/champiñones.png',
+    bio:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium sequi fuga delectus quos expedita ut ipsam similique et ea, repellend',
+  },
+  {
+    name: 'L`hort d´en Lluis',
+    productName: 'Frutas',
+    img: 'assets/img/platano.png',
+    bio:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium sequi fuga delectus quos expedita ut ipsam similique et ea, repellend',
+  },
+];
