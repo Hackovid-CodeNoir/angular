8b369040f056597dd43e828c6d22309f8752115a
ERNI\cagi <gilbe.cao@gmail.com>
2020-04-08 15:34:06 +0200

    add google map component with mock data

---
+++ projects/market-kzero/src/app/map-view/product-map/markers.ts
@@ -0,0 +1,14 @@
+export const markers = [
+  {
+    position: new google.maps.LatLng(41.3819571, 2.1719914),
+    title: 'La Boquería',
+  },
+  {
+    position: new google.maps.LatLng(41.370973, 2.170923),
+    title: 'Montjuic',
+  },
+  {
+    position: new google.maps.LatLng(41.388991, 2.16059),
+    title: 'Letamendi',
+  },
+];
--- projects/market-kzero/src/app/map-view/product-map/product-map.component.ts
+++ projects/market-kzero/src/app/map-view/product-map/product-map.component.ts
@@ -1,12 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { markers } from './markers';
 
 @Component({
   selector: 'app-product-map',
   templateUrl: './product-map.component.html',
   styleUrls: ['./product-map.component.scss'],
 })
-export class ProductMapComponent implements OnInit {
-  constructor() {}
+export class ProductMapComponent implements AfterViewInit {
+  @ViewChild('mapContainer', { static: false }) gmap: ElementRef;
+  lat = 41.387124;
+  lng = 2.170037;
+  map: google.maps.Map;
+  coordinates = new google.maps.LatLng(this.lat, this.lng);
+  mapOptions: google.maps.MapOptions = {
+    center: this.coordinates,
+    zoom: 14,
+    scrollwheel: false,
+  };
 
-  ngOnInit(): void {}
+  markers = markers;
+
+  ngAfterViewInit() {
+    this.mapInitializer();
+  }
+
+  mapInitializer() {
+    this.map = new google.maps.Map(this.gmap.nativeElement, this.mapOptions);
+    this.markers.forEach((markerInfo) => {
+      const marker = new google.maps.Marker({ ...markerInfo });
+      marker.setMap(this.map);
+    });
+  }
 }
