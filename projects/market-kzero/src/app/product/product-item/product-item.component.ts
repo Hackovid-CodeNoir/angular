1c5f12a4505088789abdbe89af1661c1598c3a32
ERNI\cagi <gilbe.cao@gmail.com>
2020-04-08 16:15:55 +0200

    improve card tile, remove unneded code from class

--- projects/market-kzero/src/app/product/product-item/product-item.component.ts
+++ projects/market-kzero/src/app/product/product-item/product-item.component.ts
@@ -1,13 +1,10 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-product-item',
   templateUrl: './product-item.component.html',
   styleUrls: ['./product-item.component.scss'],
 })
-export class ProductItemComponent implements OnInit {
-  @Input() seller: any = {};
-  constructor() {}
-
-  ngOnInit(): void {}
+export class ProductItemComponent {
+  @Input() seller: any;
 }
