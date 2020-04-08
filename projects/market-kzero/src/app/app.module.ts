9db8dbf51bac2eb31f6dd4819606accc746f3379
Leonides Muguercia Tellez <leonimuguercia5@gmail.com>
2020-04-08 09:22:51 +0200

    Creating Product module & product list & product item components

--- projects/market-kzero/src/app/app.module.ts
+++ projects/market-kzero/src/app/app.module.ts
@@ -5,10 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { GlobalAreaModule } from './global-area/global-area.module';
+import { ProductModule } from './product/product.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, GlobalAreaModule],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, GlobalAreaModule, ProductModule],
   providers: [],
   bootstrap: [AppComponent],
 })
