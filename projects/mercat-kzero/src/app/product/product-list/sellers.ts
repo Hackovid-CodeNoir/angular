16d6e6cc7501ff532f13521f540a5936b822064b
Leonides Muguercia Tellez <leonimuguercia5@gmail.com>
2020-04-09 22:07:18 +0200

    Improving the product item, adding the surprise basket and styling the product card

--- projects/mercat-kzero/src/app/product/product-list/sellers.ts
+++ projects/mercat-kzero/src/app/product/product-list/sellers.ts
@@ -2,7 +2,7 @@ export const sellers = [
   {
     id: 0,
     name: 'L´hort de Lourdes',
-    productName: 'Cistelles ecológiques',
+    productName: 'varied basket',
     img: 'assets/img/cesta.png',
     bio:
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium sequi fuga delectus quos expedita ut ipsam similique et ea, repellend',
@@ -11,37 +11,10 @@ export const sellers = [
   {
     id: 1,
     name: 'Can Manel',
-    productName: 'Fruitas i Verdures',
-    img: 'assets/img/frutas.png',
+    productName: 'surprise basket',
+    img: 'assets/img/cesta-1.2.png',
     bio:
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium sequi fuga delectus quos expedita ut ipsam similique et ea, repellend',
     rating: 2,
-  },
-  {
-    id: 2,
-    name: 'Verdures Carmen',
-    productName: 'Verdures variades',
-    img: 'assets/img/zanahorias.png',
-    bio:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium sequi fuga delectus quos expedita ut ipsam similique et ea, repellend',
-    rating: 4,
-  },
-  {
-    id: 3,
-    name: 'La Masia',
-    productName: 'Llegums i bollets',
-    img: 'assets/img/champiñones.png',
-    bio:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium sequi fuga delectus quos expedita ut ipsam similique et ea, repellend',
-    rating: 3,
-  },
-  {
-    id: 4,
-    name: 'L`hort d´en Lluis',
-    productName: 'Frutas',
-    img: 'assets/img/platano.png',
-    bio:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium sequi fuga delectus quos expedita ut ipsam similique et ea, repellend',
-    rating: 5,
   },
 ];
