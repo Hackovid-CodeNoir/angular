d1630cc57635ba55224fbe89b5cd15d54969e7ea
Leonides Muguercia Tellez <leonimuguercia5@gmail.com>
2020-04-10 16:21:31 +0200

    Creating description module and description component, deleting product component fromn app.module

--- projects/mercat-kzero/src/app/main-content/main-content.module.ts
+++ projects/mercat-kzero/src/app/main-content/main-content.module.ts
@@ -4,9 +4,10 @@ import { MainContentComponent } from './main-content/main-content.component';
 import { ProductModule } from '../product/product.module';
 import { MapViewModule } from '../map-view/map-view.module';
 import { MainContentRoutingModule } from './main-content-routing.module';
+import { DescriptionModule } from '../description/description.module';
 
 @NgModule({
   declarations: [MainContentComponent],
-  imports: [CommonModule, ProductModule, MapViewModule, MainContentRoutingModule],
+  imports: [CommonModule, ProductModule, MapViewModule, MainContentRoutingModule, DescriptionModule]
 })
 export class MainContentModule {}
