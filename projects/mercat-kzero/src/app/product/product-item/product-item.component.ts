47355630321f039ecb549386afb8cc3ee81df0b8
ERNI\cagi <gilbe.cao@gmail.com>
2020-04-10 03:08:42 +0200

    transform basket selector

--- projects/mercat-kzero/src/app/product/product-item/product-item.component.ts
+++ projects/mercat-kzero/src/app/product/product-item/product-item.component.ts
@@ -1,5 +1,4 @@
 import { Component, Input } from '@angular/core';
-import { sellers } from '../product-list/sellers';
 
 @Component({
   selector: 'app-product-item',
@@ -8,15 +7,4 @@ import { sellers } from '../product-list/sellers';
 })
 export class ProductItemComponent {
   @Input() seller: any;
-  public sellers = sellers;
-
-  ratingClicked: number;
-
-  ratingComponentClick(clickObj: any): void {
-    const item = this.sellers.find((seller: any) => seller.id === clickObj.itemId);
-    if (!!item) {
-      item.rating = clickObj.rating;
-      this.ratingClicked = clickObj.rating;
-    }
-  }
 }
--- projects/mercat-kzero/src/app/product/product-list/sellers.ts
+++ projects/mercat-kzero/src/app/product/product-list/sellers.ts
@@ -2,7 +2,7 @@ export const sellers = [
   {
     id: 0,
     name: 'L´hort de Lourdes',
-    productName: 'varied basket',
+    productName: 'Varied Basket',
     img: 'assets/img/cesta.png',
     bio:
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium sequi fuga delectus quos expedita ut ipsam similique et ea, repellend',
@@ -11,7 +11,7 @@ export const sellers = [
   {
     id: 1,
     name: 'Can Manel',
-    productName: 'surprise basket',
+    productName: 'Surprise Basket',
     img: 'assets/img/cesta-1.2.png',
     bio:
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium sequi fuga delectus quos expedita ut ipsam similique et ea, repellend',
